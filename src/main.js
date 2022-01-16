b3232d8c4eb37b45a46366e99874cc8cea53a4b4
Neopapa94 <neoptolemos.papadiofantous@gmail.com>
2022-01-16 20:22:48 +0000

    Added package dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vuex from 'vuex'
+
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+//Vuex 
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
